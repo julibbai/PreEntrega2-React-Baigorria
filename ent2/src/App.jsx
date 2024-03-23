87fb16dc33091158203edcd125ca291272ce9b42
julibbai <julietabbaigorria@gmail.com>
2024-03-23 20:55:54 -0300

    terminado2

--- ent2/src/App.jsx
+++ ent2/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import ItemListContainer from "./components/pages/ItemListContainer";
 import { Cart } from "./components/common";
@@ -8,13 +8,27 @@ import { CheckoutContainer } from "./components/pages/checkout/CheckoutContainer
 import { CartContainer } from "./components/pages/cart/CartContainer";
 import CartContextProvider from "./context/CartContext";
 import { AppRouter } from "./router/AppRouter";
+import { Layout } from "./components/layout/Layout";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <CartContextProvider>
-          <AppRouter />
+          {/* <AppRouter /> */}
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route
+                path="/category/:category"
+                element={<ItemListContainer />}
+              />
+              <Route path="/cart" element={<CartContainer />} />
+              <Route path="/item/:id" element={<ItemDetailContainer />} />
+              <Route path="/checkout" element={<CheckoutContainer />} />
+            </Route>
+            <Route path="*" element={<h1>404 not found</h1>} />
+          </Routes>
         </CartContextProvider>
       </BrowserRouter>
     </>
